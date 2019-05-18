07573daf8774b8d5da0ffc665ba2562350bbd447
kla101 <kla101@uclive.ac.nz>
2019-05-19 11:31:28 +1200

    Added posting map

--- app/src/main/java/com/example/termtwoproject/MapApiHandler.kt
+++ app/src/main/java/com/example/termtwoproject/MapApiHandler.kt
@@ -3,8 +3,10 @@ package com.example.termtwoproject
 import android.os.AsyncTask
 import com.example.termtwoproject.models.GpsMap
 import com.example.termtwoproject.models.PostModel
+import com.google.android.gms.common.util.JsonUtils
 import org.json.JSONObject
 import java.io.BufferedInputStream
+import java.io.DataOutputStream
 import java.net.HttpURLConnection
 import java.net.URL
 import java.nio.charset.Charset
@@ -20,7 +22,8 @@ class MapApiHandler(val callback : (Int) -> Unit): AsyncTask<PostModel, Void, In
             println(result)
         } else {
             // method is for saving or updating a gps map
-            postOrPatchMap(m)
+            val result = postOrPatchMap(m)
+            println(result)
         }
         return 1
     }
@@ -44,7 +47,13 @@ class MapApiHandler(val callback : (Int) -> Unit): AsyncTask<PostModel, Void, In
         with(model.url.openConnection() as HttpURLConnection) {
             requestMethod = model.method
             setRequestProperty("Content-Type", "application/json")
+            val postData : ByteArray = model.map!!.toJSON().toString().toByteArray(Charset.defaultCharset())
 
+            val outputStream : DataOutputStream = DataOutputStream(outputStream)
+            outputStream.write(postData)
+            outputStream.flush()
+            val json = BufferedInputStream(inputStream).readBytes().toString(Charset.defaultCharset())
+            return JSONObject(json)
         }
     }
 }
--- app/src/main/java/com/example/termtwoproject/models/GpsMap.kt
+++ app/src/main/java/com/example/termtwoproject/models/GpsMap.kt
@@ -1,11 +1,43 @@
 package com.example.termtwoproject.models
 
+import android.util.JsonWriter
+import org.json.JSONArray
 import org.json.JSONObject
-
+import java.io.OutputStreamWriter
 
 
 data class GpsMap(val id : Int, val title : String, val type : String, val fragmentAmount : Int, val category : String,
                   var votes : Int, val fragments : List<Fragment>) {
+
+
+    fun toJSON() : JSONObject {
+        val rootObject = JSONObject()
+        rootObject.put("id", id)
+        rootObject.put("title", title)
+        rootObject.put("mapType", type)
+        rootObject.put("fragmentAmount", fragmentAmount)
+        rootObject.put("category", category)
+        rootObject.put("votes", votes)
+        val fragmentArray = JSONArray()
+        fragments.forEach {
+            val jsonFragment = JSONObject()
+            jsonFragment.put("lineColour", it.lineColour)
+            jsonFragment.put("zoom", it.zoom)
+            val jsonCoordinates = JSONArray()
+            it.coordinates.forEach { seq ->
+                val jsonCoord = JSONObject()
+                jsonCoord.put("sequence", seq.sequence)
+                jsonCoord.put("longitude", seq.longitude)
+                jsonCoord.put("latitude", seq.latitude)
+                jsonCoordinates.put(jsonCoord)
+            }
+            jsonFragment.put("coordinates", jsonCoordinates)
+            fragmentArray.put(jsonFragment)
+        }
+        rootObject.put("fragments", fragmentArray)
+        return rootObject
+    }
+
 
 
     companion object {
