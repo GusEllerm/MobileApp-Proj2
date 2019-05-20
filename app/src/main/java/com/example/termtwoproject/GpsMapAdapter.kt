275d5d08621ec4569346f18ed884e6fd22a87df4
kla101 <kla101@uclive.ac.nz>
2019-05-20 20:12:25 +1200

    GoogleMap implementation into global drawings.

--- app/src/main/java/com/example/termtwoproject/GpsMapAdapter.kt
+++ app/src/main/java/com/example/termtwoproject/GpsMapAdapter.kt
@@ -6,6 +6,9 @@ import android.view.ViewGroup
 import android.widget.Button
 import androidx.recyclerview.widget.RecyclerView
 import com.example.termtwoproject.models.PostModel
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.LatLng
 import java.net.URL
 
 class GpsMapAdapter(private val context: Context): RecyclerView.Adapter<GpsMapViewHolder>() {
@@ -34,11 +37,17 @@ class GpsMapAdapter(private val context: Context): RecyclerView.Adapter<GpsMapVi
         val mapId : Int = mapIds[i]
         val query = "?id=$mapId"
         val url = URL(AppConstants.GPS_END + query)
+
+        val googleMap : GoogleMap = holder.currentMap
+
         MapDownloader {
             holder.mapCategoryText.text = it.category
             holder.mapTitleText.text = it.title
             holder.mapVotesText.text = it.votes.toString()
         }.execute(url)
+
+        val sydney = LatLng(-34.0, 151.0)
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
 
     }
 
--- app/src/main/java/com/example/termtwoproject/GpsMapViewHolder.kt
+++ app/src/main/java/com/example/termtwoproject/GpsMapViewHolder.kt
@@ -3,9 +3,27 @@ package com.example.termtwoproject
 import android.view.View
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.OnMapReadyCallback
 
-class GpsMapViewHolder(drawingRow : View) : RecyclerView.ViewHolder(drawingRow) {
+class GpsMapViewHolder(drawingRow : View) : RecyclerView.ViewHolder(drawingRow), OnMapReadyCallback {
     val mapCategoryText = drawingRow.findViewById<TextView>(R.id.rowCategoryText)
     val mapTitleText = drawingRow.findViewById<TextView>(R.id.rowTitleText)
     val mapVotesText = drawingRow.findViewById<TextView>(R.id.rowVotesText)
+    private val mapView = drawingRow.findViewById<MapView>(R.id.mapView)
+    lateinit var currentMap : GoogleMap
+
+    init {
+        if (mapView != null) {
+            mapView.onCreate(null)
+            mapView.onResume()
+            mapView.getMapAsync(this)
+        }
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        currentMap = googleMap
+    }
+
 }
