11daf97e026e392a0a0430eee30604cace607668
Kevin Langbroek <kla101@uclive.ac.nz>
2019-05-22 12:20:19 +1200

    small changes

--- app/src/main/java/com/example/termtwoproject/DrawActivity.kt
+++ app/src/main/java/com/example/termtwoproject/DrawActivity.kt
@@ -19,10 +19,7 @@ import android.widget.Toast
 import androidx.room.Room
 import com.example.termtwoproject.Database.Drawing
 import com.example.termtwoproject.Database.DrawingsDatabase
-import com.example.termtwoproject.Dialogues.CreateNewLineDialog
-import com.example.termtwoproject.Dialogues.DeleteLineDialog
-import com.example.termtwoproject.Dialogues.EditLineDialog
-import com.example.termtwoproject.Dialogues.ViewLineDialog
+import com.example.termtwoproject.Dialogues.*
 import com.google.android.gms.common.api.ResolvableApiException
 import com.google.android.gms.location.*
 
@@ -40,7 +37,7 @@ import java.util.Collections.max
 
 
 class DrawActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener, CreateNewLineDialog.NewLineDialogListner,
-EditLineDialog.EditDialogListener, ViewLineDialog.ViewLineDialogListener, DeleteLineDialog.DeleteLineDialogListner {
+EditLineDialog.EditDialogListener, ViewLineDialog.ViewLineDialogListener, DeleteLineDialog.DeleteLineDialogListner, UploadLineDialog.UploadLineDialogListener {
 
     private lateinit var map: GoogleMap
     private lateinit var fusedLocationClient: FusedLocationProviderClient
@@ -140,12 +137,18 @@ EditLineDialog.EditDialogListener, ViewLineDialog.ViewLineDialogListener, Delete
         Toast.makeText(this, "Edit selected Fragment", Toast.LENGTH_SHORT).show()
     }
 
+    override fun uploadFragment() {
+        stopRecording()
+
+    }
+
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
         when (item?.itemId) {
             R.id.New -> openNewDialog()
             R.id.View -> openViewDialog()
             R.id.Edit -> openEditDialog()
             R.id.Delete -> openDeleteDialog()
+
         }
         return super.onOptionsItemSelected(item)
     }
